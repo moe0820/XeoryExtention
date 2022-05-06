95c16c8c0a5d5e71e2c8c2826417d5ca15145ba8
moe0820 <moe0820.tb@gmail.com>
2022-05-07 01:33:31 +0900

    js追加：スクロールのjsを追加

---
+++ script.js
@@ -0,0 +1,12 @@
+
+window.addEventListener("scroll", function () {
+  // スクロールしたピクセル数を取得
+  let scroll = document.documentElement.scrollTop;
+  // document.getElementById("scrollValue").textContent = scroll;
+
+  if (scroll < 200) {
+    this.document.querySelector(".pagetop").classList.add("display")
+  } else {
+    this.document.querySelector(".pagetop").classList.remove("display");
+  }
+});
