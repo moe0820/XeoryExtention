ab0c2b25319a1e74fe545df971396d1736180559
moe0820 <moe0820.tb@gmail.com>
2022-05-15 18:14:34 +0900

    css追加：バーガーメニュー

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ window.addEventListener("scroll", function () {
   // document.getElementById("scrollValue").textContent = scroll;
 
   if (scroll < 200) {
-    this.document.querySelector(".pagetop").classList.add("display")
+    this.document.querySelector(".pagetop").classList.add("display");
   } else {
     this.document.querySelector(".pagetop").classList.remove("display");
   }
+});
+// ドロワー
+const menu = document.querySelector(".menu");
+const drawer = document.querySelector(".drawer");
+
+menu.addEventListener("click", () => {
+  drawer.classList.toggle("on");
 });
